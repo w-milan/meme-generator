fa05033f129674752e180cbc3a3874f5b8962d20
milan <pperisic.milan@gmail.com>
2022-10-26 23:15:34 +0200

    Making random meme

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,6 +1,17 @@
 import React from 'react'
+import memesData from '../memesData'
 
 const Meme = () => {
+
+  function getMemeImage(e){
+    e.preventDefault();
+
+    const memesArray = memesData.data.memes
+    const randomNumber = Math.floor(Math.random() * memesArray.length)   
+    const url = memesArray[randomNumber].url 
+    console.log(url)  
+  }
+
   return (
     <main>
       <form className='form'>
@@ -20,7 +31,7 @@ const Meme = () => {
           className='form-input'
         >
         </input>
-        <button className='form-button'>Get a new meme image 🖼</button>
+        <button onClick={getMemeImage} className='form-button'>Get a new meme image 🖼</button>
       </form>
     </main>
   )

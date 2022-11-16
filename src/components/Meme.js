3db46903922dd499ce29463abf9fa1a5c3576ea8
milan <pperisic.milan@gmail.com>
2022-11-16 19:12:27 +0100

    Make function to take memes from API

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import memesData from '../memesData'
 
 const Meme = () => {
 
@@ -9,14 +8,19 @@ const Meme = () => {
     randomImage: "http://i.imgflip.com/1bij.jpg"
   })
 
-  const [allMemeImages, setAllMemeImages] = React.useState(memesData)
+  const [allMemes, setAllMemes] = React.useState([])
+
+  React.useEffect(() => {
+    fetch("https://api.imgflip.com/get_memes")
+        .then(res => res.json())
+        .then(data => setAllMemes(data.data.memes))
+}, [])
 
   function getMemeImage(e){
     e.preventDefault();
 
-    const memesArray = allMemeImages.data.memes
-    const randomNumber = Math.floor(Math.random() * memesArray.length)   
-    const url = memesArray[randomNumber].url 
+    const randomNumber = Math.floor(Math.random() * allMemes.length)   
+    const url = allMemes[randomNumber].url 
     setMeme(prevMeme => ({
       ...prevMeme,
       randomImage: url
